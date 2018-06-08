b939660a4f02f359615cf74208a66cb542945782
Santipong Jarat <santipongjarat@gmail.com>
2018-06-08 15:41:27 +0700

    fix bug RemotePDF zoom crash
    fix bug RemotePDF zoom crash

--- library/src/main/java/es/voghdev/pdfviewpager/library/RemotePDFViewPager.java
+++ library/src/main/java/es/voghdev/pdfviewpager/library/RemotePDFViewPager.java
@@ -20,6 +20,7 @@ import android.content.res.TypedArray;
 import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 import java.io.File;
 
@@ -109,6 +110,16 @@ public class RemotePDFViewPager extends ViewPager implements DownloadFile.Listen
 
         public void onProgressUpdate(int progress, int total) {
             /* Empty */
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 }
